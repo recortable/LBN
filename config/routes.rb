15cd6dd6aaa89685bb1abb6bea34dbf0fd8b37ac
Danigb <danigb@gmail.com>
2011-03-12 07:15:10 +0100

    act

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,29 @@
 LBN::Application.routes.draw do
 
-#  root :to => 'un_mal_dia#portada'
+  root :to => 'un_mal_dia#portada'
 
-#  match 'un-mal-dia-lo-tiene-cualquiera' => 'un_mal_dia#unmaldia', :as => :unmaldia
-#  match 'canciones-un-mal-dia' => 'un_mal_dia#canciones', :as => :canciones
-#  match 'el-fin-esta-cerca' => 'un_mal_dia#reloj', :as => :reloj
-#  match 'videos' => 'un_mal_dia#videos', :as => :videos
-#  match 'sabe-mi-nombre' => 'un_mal_dia#lanada', :as => :lanada
+  match 'un-mal-dia-lo-tiene-cualquiera' => 'un_mal_dia#unmaldia', :as => :unmaldia
+  match 'canciones-un-mal-dia' => 'un_mal_dia#canciones', :as => :canciones
+  match 'el-fin-esta-cerca' => 'un_mal_dia#reloj', :as => :reloj
+  match 'videos' => 'un_mal_dia#videos', :as => :videos
+  match 'sabe-mi-nombre' => 'un_mal_dia#lanada', :as => :lanada
 
-#  match 'aventuras-domesticas' => 'aventuras#cortinas', :as => :aventuras
-#  match 'aventura-domestica' => 'aventuras#portada', :as => :aventuras_portada
-#  match 'canciones-aventuras-domesticas' => 'aventuras#canciones', :as => :canciones_aventuras
+  match 'aventuras-domesticas' => 'aventuras#cortinas', :as => :aventuras
+  match 'aventura-domestica' => 'aventuras#portada', :as => :aventuras_portada
+  match 'canciones-aventuras-domesticas' => 'aventuras#canciones', :as => :canciones_aventuras
 
-#  AventurasController::CANCIONES.each do |cancion|
-#    match cancion => 'aventuras#cancion', :defaults => {:title => cancion}
-#  end
+  AventurasController::CANCIONES.each do |cancion|
+    match cancion => 'aventuras#cancion', :defaults => {:title => cancion}
+  end
 
 
-#  resources :comments
+  resources :comments
 
-#  match 'admin' => 'admin/comments#index', :as => 'admin'
-#  match 'emails' => 'admin/comments#emails', :as => 'admin_emails'
-#  namespace :admin do
-#    resources :comments
-#  end
+  match 'admin' => 'admin/comments#index', :as => 'admin'
+  match 'emails' => 'admin/comments#emails', :as => 'admin_emails'
+  namespace :admin do
+    resources :comments
+  end
 
 
 end
