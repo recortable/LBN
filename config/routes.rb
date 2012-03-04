e8cf153a4a3dcfae0969e01236bf02736d90aa49
danigb <danigb@gmail.com>
2012-03-04 15:18:23 +0100

    Discos

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,21 @@ LBN::Application.routes.draw do
 
   root :to => 'un_mal_dia#portada'
 
+  # UN MAL DÍA
   match 'un-mal-dia-lo-tiene-cualquiera' => 'un_mal_dia#unmaldia', :as => :unmaldia
   match 'canciones-un-mal-dia' => 'un_mal_dia#canciones', :as => :canciones
   match 'el-fin-esta-cerca' => 'un_mal_dia#reloj', :as => :reloj
   match 'videos' => 'un_mal_dia#videos', :as => :videos
   match 'sabe-mi-nombre' => 'un_mal_dia#lanada', :as => :lanada
 
+  # AVENTURAS DOMÉSTICAS
   match 'aventuras-domesticas' => 'aventuras#cortinas', :as => :aventuras
   match 'aventura-domestica' => 'aventuras#portada', :as => :aventuras_portada
   match 'canciones-aventuras-domesticas' => 'aventuras#canciones', :as => :canciones_aventuras
   match 'enlace' => 'un_mal_dia#link', :as => :link
-  match 'descargas' => 'site#downloads', :as => :downloads
+
+  # GENERAL
+  match 'discos' => 'site#downloads', :as => :downloads
   match 'nuevo-disco' => 'site#announcement'
 
   AventurasController::CANCIONES.each do |cancion|
